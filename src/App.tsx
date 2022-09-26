9753ea0a3aae9ff15a92eb44b12a019849fa242e
fortune710 <fortune0208@yahoo.com>
2022-09-26 10:24:47 +0100

    created custom theme and set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
 
+import { LoginPage } from './pages'
 function App() {
-  const [count, setCount] = useState(0)
+  const [authenticated, setAuth] = useState(false);
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#F27F7E', //Red
+      },
+      secondary: {
+        main: '#D9D9D9' //Grey
+      },
+    }
+  })
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path='/' element={authenticated ? <HomePage/> : <LoginPage/>}  />
+      </Routes>
+    </ThemeProvider>
   )
+}
+
+function HomePage(){
+  return(<div>Home page</div>)
 }
 
 export default App
