c376664ed323dc04f74a6a92ff54c615b5fcbadb
fortune710 <fortune0208@yahoo.com>
2022-09-26 10:26:31 +0100

    complted loading screen

--- src/components/LoadingScreen/loading-screen.tsx
+++ src/components/LoadingScreen/loading-screen.tsx
@@ -1,5 +1,21 @@
-export const LoadingScreen:React.FC = () => {
+import { CircularProgress } from "@mui/material";
+import { LinearProgress } from "@mui/material";
+
+type LoadingScreenProps = {
+    progressType: 'circular'|'linear',
+    message?:string
+}
+
+export const LoadingScreen:React.FC<LoadingScreenProps> = ({ progressType, message }) => {
     return(
-        <div>Loading...</div>
+        <main>
+            <div>
+            {
+                progressType === 'circular'? <CircularProgress/>
+                :progressType === 'linear' ? <LinearProgress/>
+                : <CircularProgress/>
+            }
+            </div>
+        </main>
     )
 }
