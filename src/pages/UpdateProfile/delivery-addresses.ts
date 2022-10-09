19bd95bef1d121a5c1584c8b726c12a6ecb7e203
fortune710 <fortune0208@yahoo.com>
2022-10-09 18:24:45 +0100

    mock data for delivery address

---
+++ src/pages/UpdateProfile/delivery-addresses.ts
@@ -0,0 +1,9 @@
+export const Addresses = [
+    {
+        name: "Charles Arowolo",
+        phone_number: "08085731403",
+        street_address: "No 2, Danube Street, off NUN Street",
+        city: "Maitama",
+        state: "Federal Capital Territory"
+    }
+] as const;
