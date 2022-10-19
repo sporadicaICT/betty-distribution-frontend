4d864af1a169d358b603d194aef0fa4d3f58a08a
fortune710 <fortune0208@yahoo.com>
2022-10-19 01:26:10 +0100

    modified image picker

--- src/components/ImagePicker/imagepicker.tsx
+++ src/components/ImagePicker/imagepicker.tsx
@@ -9,6 +9,22 @@ type ImagePickerProps = {
 export const ImagePicker: React.FC<ImagePickerProps> = ({ name, onChange }) => {
     const inputRef = useRef<HTMLInputElement>(null);
 
+    let base64String:any = '';
+
+    //Convert Image from input[type="file"] to base64 string
+    const toBase64 = (file:any) => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+
+    const getImage = async (event:React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.item(0)
+        base64String = await toBase64(file)
+    }
+    
+
     return(
         <Box sx={{ width:'100%', display:'flex', alignItems:'center', justifyContent:'space-between' }}>
             <TextField variant='standard' sx={{border:0}}/>
