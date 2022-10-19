a34396f2e3e530afec58d5469491f620bcdac3eb
fortune710 <fortune0208@yahoo.com>
2022-10-19 01:20:36 +0100

    created custom modal component

---
+++ src/components/CustomModal/modal.tsx
@@ -0,0 +1,18 @@
+import { Box, Modal } from "@mui/material";
+import { modalStyle, backdropStyle } from "../../utils/styles.utils";
+
+type CustomModalProps = {
+    isOpen: boolean,
+    dismiss: any, //Called when the modal is dismissed
+    children: React.ReactNode
+}
+
+export const CustomModal: React.FC<CustomModalProps> = ({ isOpen, dismiss, children }) => {
+    return(
+        <Modal sx={backdropStyle} open={isOpen} onClose={dismiss}>
+            <Box sx={modalStyle}>
+                { children }
+            </Box>
+        </Modal>
+    )
+}
