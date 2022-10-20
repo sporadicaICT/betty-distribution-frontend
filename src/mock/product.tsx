ebde1ae4349909e564099c0dc3f7f16105b7a3b9
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2022-10-20 15:36:47 +0100

    fake data for products

---
+++ src/mock/product.tsx
@@ -0,0 +1,91 @@
+import { Product } from "../types";
+import { makeid } from "../helpers";
+
+import Gino from "../assets/logo.svg"
+
+export const Product: Product = {
+    id: makeid(15),
+    name: "Gino Pepper & Onion Tomato Paste 70g Sachet",
+    brand: "gino",
+    size: "70g",
+    unit_price: 150,
+    description: "lorem ipsum bla bla bla no be me go waste time dey write description for product wey no exist lol",
+    quanitity_left: 5,
+    tags: ['spices','condiments'],
+    image_url: Gino
+}
+
+export const Products: Product[] = [
+    {
+        id: makeid(15),
+        name: "Gino Pepper & Onion Tomato Paste 70g Sachet",
+        brand: "gino",
+        size: "70g",
+        unit_price: 150,
+        description: "lorem ipsum bla bla bla no be me go waste time dey write description for product wey no exist lol",
+        quanitity_left: 5,
+        tags: ['spices','condiments'],
+        image_url: Gino
+    },
+    {
+        id: makeid(15),
+        name: "Gino Curry Powder 5g Sachet",
+        brand: "gino",
+        size: "5g",
+        unit_price: 20,
+        description: "lorem ipsum bla bla bla no be me go waste time dey write description for product wey no exist lol",
+        quanitity_left: 51,
+        tags: ['spices','condiments'],
+        image_url: Gino
+
+    },
+    {
+        id: makeid(15),
+        name: "Gino Pepper & Onion Tomato Paste 70g Sachet",
+        brand: "gino",
+        size: "70g",
+        unit_price: 150,
+        description: "lorem ipsum bla bla bla no be me go waste time dey write description for product wey no exist lol",
+        quanitity_left: 5,
+        tags: ['spices','condiments'],
+        image_url: Gino
+
+    },
+    {
+        id: makeid(15),
+        name: "Ayoola Poundo Yam Flour 900g Bag",
+        brand: "ayoola foods",
+        size: "900g",
+        unit_price: 2500,
+        description: "lorem ipsum bla bla bla no be me go waste time dey write description for product wey no exist lol",
+        quanitity_left: 5,
+        tags: ['spices','condiments'],
+        image_url: Gino
+
+    },
+    {
+        id: makeid(15),
+        name: "Knorr Chicken Cubes",
+        brand: "knorr",
+        size: "700g",
+        unit_price: 750,
+        description: "lorem ipsum bla bla bla no be me go waste time dey write description for product wey no exist lol",
+        quanitity_left: 5,
+        tags: ['spices','condiments'],
+        image_url: Gino
+
+    },
+    {
+        id: makeid(15),
+        name: "Golden Penny Spaghetti",
+        brand: "golden penny",
+        size: "700g",
+        unit_price: 200,
+        description: "lorem ipsum bla bla bla no be me go waste time dey write description for product wey no exist lol",
+        quanitity_left: 5,
+        tags: ['spices','condiments'],
+        image_url: Gino
+
+    },
+
+]
