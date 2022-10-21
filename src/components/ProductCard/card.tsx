e713ddc05fd1df977d9f0ce6026d3e3cd9d1e09f
fortune710 <fortune0208@yahoo.com>
2022-10-21 22:37:48 +0100

    completed basic styling

--- src/components/ProductCard/card.tsx
+++ src/components/ProductCard/card.tsx
@@ -14,17 +14,17 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     return(
         <div className={styles.productCard}>
             <div className={styles.imageHolder}>
-                <Box sx={{ display: 'flex', justifyContent:'flex-end' }}>
-                    <IconButton className={styles.addBtn}>
-                        <Add/>
-                    </IconButton>
+                <Box sx={{ display: 'flex', justifyContent:'flex-end', padding: '7px 10px 0 0', boxSizing: 'border-box' }}>
+                    <span className={styles.addBtn}>
+                        <Add fontSize="small"/>
+                    </span>
                 </Box>
                 <img src={product.image_url} alt={`${product.name}`}/>
             </div>
             <span>
                 <p>{product.name}</p>
                 <Typography variant="h2">
-                    {product.unit_price}
+                     <span>&#8358;</span>{product.unit_price}
                 </Typography>
             </span>
         </div>
