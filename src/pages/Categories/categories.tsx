d60c3a98a3a4621f0c7edf504f9c7a610258e317
fortune710 <fortune0208@yahoo.com>
2022-10-21 22:38:00 +0100

    finished page

--- src/pages/Categories/categories.tsx
+++ src/pages/Categories/categories.tsx
@@ -3,18 +3,29 @@ import { Navbar, ProductCard } from "../../components";
 import styles from "./categories.module.scss";
 
 import { Products } from "../../mock/product";
+import { tags } from '../../helpers';
+import { Typography } from "@mui/material";
 
 export const CategoriesPage: React.FC = () => {
-    const [selectedBrand, setSelectedBrand] = useState<string|undefined>(undefined);
+    const [selectedCategory, setSelectedCategory] = useState<string|undefined>(undefined);
 
     return(
         <>
             <Navbar signedIn={true}/>
             <main className={styles.mainContent}>
-                <BrandsList />
+                <CategoriesList 
+                    activeCategory={selectedCategory} 
+                    changeActiveCategory={setSelectedCategory}
+                />
                 <div className={styles.productsList}>
-                    {
+                    {   
+                        //if Selected category is undefined, show all products
+                        !selectedCategory ? 
                         Products.map(product => (
+                            <ProductCard product={product}/>
+                        )) 
+                        //else show category specific products
+                        : Products.filter(product => product.tags?.includes(selectedCategory)).map(product => (
                             <ProductCard product={product}/>
                         ))
                     }
@@ -25,8 +36,27 @@ export const CategoriesPage: React.FC = () => {
     )
 }
 
-const BrandsList: React.FC = () => {
+type CategoriesListProps = {
+    activeCategory?: string;
+    changeActiveCategory: React.Dispatch<React.SetStateAction<string | undefined>>;
+}
+
+const CategoriesList: React.FC<CategoriesListProps> = (props) => {
+    const { activeCategory, changeActiveCategory } = props;
     return(
-        <div></div>
+        <div className={styles.categoriesList}>
+            <Typography variant="h6">Our Categories</Typography>
+            <ul className={styles.categories}>
+            {
+                tags.map(tag => (
+                <li 
+                    className={`${activeCategory === tag ? styles.activeCategory : ''} ${styles.categoryItem}`}
+                    onClick={() => changeActiveCategory(tag)}>
+                    {tag}
+                </li>
+                ))
+            }
+            </ul>
+        </div>
     )
 }
